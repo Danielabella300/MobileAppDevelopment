31f27fd2f5bf5a5fd97cce2c13368945e379d36c
Daniel Abella <djabella300@gmail.com>
2020-07-24 22:55:25 -0400

    finished mainActivity

--- app/src/main/java/com/example/project/ListView.java
+++ app/src/main/java/com/example/project/ListView.java
@@ -1,14 +1,14 @@
 package com.example.project;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class newReminder extends AppCompatActivity {
+import android.os.Bundle;
+
+public class ListView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_reminder);
+        setContentView(R.layout.activity_list_view);
     }
 }
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button exitButton = (Button) findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        Button loadCreateNewReminder = (Button) findViewById(R.id.createNewButton);
+        loadCreateNewReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),newReminder.class));
+            }
+        });
+
+        Button loadListOfAlarm = (Button) findViewById(R.id.listReminderButton);
+        loadListOfAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),ListView.class));
+            }
+        });
+
+        Button loadSettings = (Button) findViewById(R.id.settingsButton);
+        loadSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),ListView.class));
+            }
+        });
     }
 }
